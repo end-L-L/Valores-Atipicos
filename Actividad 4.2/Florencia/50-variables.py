037519785cfe94ab1a70a29118f45bfa88a57252
Boset <boss-x86@protonmail.com>
2025-01-31 18:19:25 -0600

    Actividad 4.2
    Florencia, Italia

---
+++ Actividad 4.2/Florencia/50-variables.py
@@ -0,0 +1,28 @@
+import pandas as pd
+
+df = pd.read_csv("listings_Florencia_Sin_Nulos.csv")
+
+# 50 variables seleccionadas
+columnas_seleccionadas = [
+    "id", "source", "name", "description", "neighborhood_overview",
+    "host_id", "host_name", "host_since", "host_location", "host_about",
+    "host_response_time", "host_response_rate", "host_acceptance_rate", "host_is_superhost",
+    "host_listings_count", "host_total_listings_count", "host_verifications", "host_identity_verified",
+    "neighbourhood_cleansed", "latitude", "longitude", "property_type", "room_type",
+    "accommodates", "bathrooms_text", "bedrooms", "beds", "amenities", "price",
+    "minimum_nights", "maximum_nights", "has_availability", "availability_30",
+    "availability_60", "availability_90", "availability_365",
+    "number_of_reviews", "number_of_reviews_ltm", "number_of_reviews_l30d",
+    "first_review", "last_review", "review_scores_rating", "review_scores_accuracy",
+    "review_scores_cleanliness", "review_scores_checkin", "review_scores_communication",
+    "review_scores_location", "review_scores_value", "instant_bookable", "reviews_per_month"
+]
+
+# Filtrar solo las columnas necesarias
+df_seleccionado = df[columnas_seleccionadas]
+
+# Verificar las primeras filas del nuevo DataFrame
+print(df_seleccionado.head())
+
+# Guardar el nuevo DataFrame en un archivo CSV
+df_seleccionado.to_csv("listings_Florencia_Seleccionado.csv", index=False)
